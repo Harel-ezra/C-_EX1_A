20924036e7e051da1aa69c08a2d091d67ff8fd96
harel <harela8@gmail.com>
2021-03-10 11:02:25 +0200

    add test, and simple cpp\hdd snowman

---
+++ snowman.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "snowman.hpp"
+using namespace std;
+
+using namespace ariel;
+std::string ariel::snowman(int n)
+{
+    std:string sm="hey, im the snoman";
+    return sm;
+}
---
+++ snowman.hpp
@@ -0,0 +1,8 @@
+#include <string>
+
+using namespace std;
+
+namespace ariel
+{
+    std::string snowman(int n);
+}
